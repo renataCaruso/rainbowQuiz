cdbc0cf4c4ca7b7e678b40780fa7d3451f1e82c8
Quezia Menezes <queziamenezessouza@gmail.com>
2023-01-21 11:05:58 -0300

    :tada: Commit inicial.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello World!
+      <br />
+      <br />
+      <br />
+      <img src="logoOpt.svg" alt="Logo Rainbow Quiz (arco-íris circular formando a letra Q)" />
+      <br />
+      <br />Rainbow Quiz!
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
